bec6d3a90d7229478e1ee588487a96fa585e2478
Luchtml <luchtml5@gmail.com>
2023-04-20 16:13:52 -0300

    first commit

---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,47 @@
+import { compare } from 'bcryptjs';
+import prismaClient from '../../prisma';
+import { sign } from 'jsonwebtoken';
+
+interface AuthRequest {
+  email: string;
+  password: string;
+}
+
+class AuthUserService {
+  async execute({ email, password }: AuthRequest) {
+    //Verificar se o email existe
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      throw new Error('User/Password not found or incorrect');
+    }
+
+    //Verificar se a senha está correta
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('User/Password not found or incorrect');
+    }
+
+    // Se deu tudo certo vamos gerar o token para usuario.
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: '30d',
+      },
+    );
+
+    return { id: user.id, name: user.name, email: user.email, token };
+  }
+}
+
+export { AuthUserService };
